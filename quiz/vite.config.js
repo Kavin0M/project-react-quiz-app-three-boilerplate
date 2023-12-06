4b06b222ef9f40efda61f9adb6c3825c502624cd
Kavin0M <kavinsai88@gmail.com>
2023-12-06 13:59:58 +0530

    Updated deploy

--- quiz/vite.config.js
+++ quiz/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],base:"/"
+  plugins: [react()],base:"/project-react-quiz-app-three-boilerplate"
 })
