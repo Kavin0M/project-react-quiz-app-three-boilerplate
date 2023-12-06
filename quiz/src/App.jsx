8eba92d3ba58dde1742b6ab6b7d260bf3244b8cf
Kavin0M <kavinsai88@gmail.com>
2023-12-06 14:05:54 +0530

    test2

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/project-react-quiz-app-three-boilerplate/" element={<Home />}/>
         <Route path='/quiz' element={<Quiz />}/>
         <Route path='/result' element={<Result />}/>
       </Routes>
